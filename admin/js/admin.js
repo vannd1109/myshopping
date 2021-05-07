cc3a393bd40a8a55d7b9d70a3c36be3a1279c0d4
vannd1109 <duynguyen1109gl@gmail.com>
2021-05-07 23:55:13 +0700

    Changed in admin

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -77,4 +77,67 @@ document.addEventListener("DOMContentLoaded", function() {
 		});
 		
 	}
+
+	// my_shopping--bar-icon
+	let bars_icon = document.querySelector("#my_shopping--bar-icon"),
+	my_shopping_header = document.querySelector("#my_shopping--header"),
+	my_shopping_content = document.querySelector("#my_shopping--content"),
+	my_shopping_content_right = document.querySelector("#my_shopping--content-right");
+
+	let count = 0;
+	bars_icon.addEventListener("click", function(){
+		count++;
+		if(count % 2 != 0){
+			my_shopping_header.classList.add('close');
+			my_shopping_content.classList.add('close');
+			my_shopping_content_right.classList.add('change_width')
+			my_shopping_header.classList.remove('open');
+			my_shopping_content.classList.remove('open');
+			my_shopping_content_right.classList.remove('remove_change_width');
+		}
+		if(count % 2 == 0){
+			my_shopping_header.classList.remove('close');
+			my_shopping_content.classList.remove('close');
+			my_shopping_content_right.classList.remove('change_width');
+			my_shopping_header.classList.add('open');
+			my_shopping_content.classList.add('open');
+			my_shopping_content_right.classList.add('remove_change_width');
+		}
+	});
+
+
+	// show_side_bar
+	let show_side_bar = document.querySelector("#my_shopping--show-side-bar"),
+		sidebar = document.querySelector("#my_shopping--content-sidebar"),
+		intro_open = document.querySelector("#my_shopping--intro-open"),
+		intro = document.querySelector("#my_shopping--intro");
+
+	let count_click = 0;
+
+	show_side_bar.addEventListener("click", function(){
+
+		count_click++;
+		if(count_click % 2 != 0){
+			sidebar.classList.add('show_side_bar');
+			sidebar.classList.remove('hide_side_bar');
+		}
+		if(count_click % 2 == 0){
+			sidebar.classList.remove('show_side_bar');
+			sidebar.classList.add('hide_side_bar');
+		}
+	});
+
+	let count_intro = 0;
+
+	intro_open.addEventListener("click", function(){
+		count_intro++;
+		if(count_intro % 2 != 0){
+			intro.classList.add('intro_show');
+			intro.classList.remove('intro_hide');
+		}
+		if(count_intro % 2 == 0){
+			intro.classList.remove('intro_show');
+			intro.classList.add('intro_hide');
+		}
+	});
 })
